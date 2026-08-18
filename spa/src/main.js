b13021e22222efafa61cd5f96570bf046d98306c
LaCringe0 <staskami440@gmail.com>
2026-08-18 19:00:24 +0200

    Delete spa directory

--- spa/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { VueDatePicker } from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
-
-createApp(App).component('VueDatePicker', VueDatePicker).mount('#app')
